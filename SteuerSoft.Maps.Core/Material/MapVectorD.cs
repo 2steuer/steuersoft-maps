780db580189cb80d9cb38b6ca1c01d5606c803e2
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2016-11-25 21:26:09 +0100

    adding mapvectord

---
+++ SteuerSoft.Maps.Core/Material/MapVectorD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteuerSoft.Maps.Core.Material
+{
+   /// <summary>
+   /// A struct representing a point in double precision floating point X/Y coordinates.
+   /// </summary>
+   public struct MapVectorD : IEquatable<MapVectorD>
+   {
+      /// <summary>
+      /// The X Coordinate
+      /// </summary>
+      public double X { get; set; }
+
+      /// <summary>
+      /// The Y coodinate.
+      /// </summary>
+      public double Y { get; set; }
+
+
+      public override bool Equals(object obj) => obj is MapVector && Equals((MapVector)obj);
+
+      public bool Equals(MapVectorD other)
+      {
+         return X == other.X &&
+                Y == other.Y;
+      }
+
+      public override int GetHashCode()
+      {
+         return 17 + X.GetHashCode() * 23 + Y.GetHashCode() * 31;
+      }
+
+      public static MapVectorD operator -(MapVectorD a, MapVectorD b)
+      {
+         return new MapVectorD() { X = a.X - b.X, Y = a.Y - b.Y };
+      }
+
+      public static MapVectorD operator +(MapVectorD a, MapVectorD b)
+      {
+         return new MapVectorD() { X = a.X + b.X, Y = a.Y + b.Y };
+      }
+
+      public static MapVectorD operator *(MapVectorD a, double scale)
+      {
+         return new MapVectorD() { X = (int)(a.X * scale), Y = (int)(a.Y * scale) };
+      }
+
+      public static MapVectorD operator *(double scale, MapVectorD a)
+      {
+         return a * scale;
+      }
+
+      public static MapVectorD operator /(MapVectorD a, double div)
+      {
+         return new MapVectorD() { X = (int)(a.X / div), Y = (int)(a.Y / div) };
+      }
+   }
+}
