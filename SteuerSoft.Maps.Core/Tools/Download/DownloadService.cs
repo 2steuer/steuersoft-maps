5f3f7ca278d913035649d75dbe4ae598cb8c9985
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2018-10-08 20:32:28 +0200

    - Fixing formatting

--- SteuerSoft.Maps.Core/Tools/Download/DownloadService.cs
+++ SteuerSoft.Maps.Core/Tools/Download/DownloadService.cs
@@ -8,91 +8,92 @@ using SteuerSoft.Maps.Core.Tools.Download.Material;
 
 namespace SteuerSoft.Maps.Core.Tools.Download
 {
-   /// <summary>
-   /// This class offers functionality to download files.
-   /// Since this is threaded, we can specify a number of threads that is paralell downloading data.
-   /// </summary>
-   public class DownloadService
-   {
-      /// <summary>
-      /// The queue of jobs that shall be downloaded.
-      /// </summary>
-      private ConcurrentQueue<DownloadJob> _jobs = new ConcurrentQueue<DownloadJob>();
+    /// <summary>
+    /// This class offers functionality to download files.
+    /// Since this is threaded, we can specify a number of threads that is paralell downloading data.
+    /// </summary>
+    public class DownloadService
+    {
+        /// <summary>
+        /// The queue of jobs that shall be downloaded.
+        /// </summary>
+        private ConcurrentQueue<DownloadJob> _jobs = new ConcurrentQueue<DownloadJob>();
 
-      /// <summary>
-      /// Mutex for the current thread counter.
-      /// </summary>
-      Mutex _threadCounterMutex = new Mutex();
+        /// <summary>
+        /// Mutex for the current thread counter.
+        /// </summary>
+        Mutex _threadCounterMutex = new Mutex();
 
-      /// <summary>
-      /// Number of currently running threads.
-      /// </summary>
-      private int _currentThreads = 0;
+        /// <summary>
+        /// Number of currently running threads.
+        /// </summary>
+        private int _currentThreads = 0;
 
-      /// <summary>
-      /// Number of threads that may be running at the same time.
-      /// </summary>
-      public int MaxThreads { get; set; } = 2;
+        /// <summary>
+        /// Number of threads that may be running at the same time.
+        /// </summary>
+        public int MaxThreads { get; set; } = 2;
 
-      /// <summary>
-      /// The HTTP User Agent that shall be sent to the webserver.
-      /// </summary>
-      public string UserAgent { get; set; }
+        /// <summary>
+        /// The HTTP User Agent that shall be sent to the webserver.
+        /// </summary>
+        public string UserAgent { get; set; }
 
-      /// <summary>
-      /// Enqueues a job to the job queue. If there are more jobs than started threads,
-      /// new threads will be started up to a number of MaxThreads threads.
-      /// </summary>
-      /// <param name="job"></param>
-      public void AddJob(DownloadJob job)
-      {
-         _jobs.Enqueue(job);
+        /// <summary>
+        /// Enqueues a job to the job queue. If there are more jobs than started threads,
+        /// new threads will be started up to a number of MaxThreads threads.
+        /// </summary>
+        /// <param name="job"></param>
+        public void AddJob(DownloadJob job)
+        {
+            _jobs.Enqueue(job);
 
-         _threadCounterMutex.WaitOne();
-         if (_currentThreads < _jobs.Count)
-         {
-            for (int i = 0; i < (_jobs.Count - _currentThreads) && _currentThreads < MaxThreads; i++)
+            _threadCounterMutex.WaitOne();
+            if (_currentThreads < _jobs.Count)
             {
-               Thread t = new Thread(DownloadThread);
-               t.Start();
-               _currentThreads++;
+                for (int i = 0; i < (_jobs.Count - _currentThreads) && _currentThreads < MaxThreads; i++)
+                {
+                    Thread t = new Thread(DownloadThread);
+                    t.Start();
+                    _currentThreads++;
+                }
             }
-         }
-         _threadCounterMutex.ReleaseMutex();
-      }
+            _threadCounterMutex.ReleaseMutex();
+        }
 
-      /// <summary>
-      /// The thread handling the download of the data.
-      /// </summary>
-      private void DownloadThread()
-      {
-         while (_jobs.Count > 0)
-         {
-            DownloadJob currentJob;
-            if (_jobs.TryDequeue(out currentJob))
+        /// <summary>
+        /// The thread handling the download of the data.
+        /// </summary>
+        private void DownloadThread()
+        {
+            while (_jobs.Count > 0)
             {
-               using (WebClient client = new WebClient())
-               {
-                  client.Headers.Add("User-Agent", UserAgent);
+                DownloadJob currentJob;
+                if (_jobs.TryDequeue(out currentJob))
+                {
+                    using (WebClient client = new WebClient())
+                    {
 
-                  MemoryStream memStr;
+                        client.Headers.Add("User-Agent", UserAgent);
 
-                  try
-                  {
-                     memStr = new MemoryStream(client.DownloadData(currentJob.Url));
-                     currentJob.SendFinishedEvent(memStr);
-                  }
-                  catch (Exception ex)
-                  {
-                     currentJob.SendFailedEvent();
-                  }
-               }
+                        MemoryStream memStr;
+
+                        try
+                        {
+                            memStr = new MemoryStream(client.DownloadData(currentJob.Url));
+                            currentJob.SendFinishedEvent(memStr);
+                        }
+                        catch (Exception ex)
+                        {
+                            currentJob.SendFailedEvent();
+                        }
+                    }
+                }
             }
-         }
 
-         _threadCounterMutex.WaitOne();
-         _currentThreads--;
-         _threadCounterMutex.ReleaseMutex();
-      }
-   }
+            _threadCounterMutex.WaitOne();
+            _currentThreads--;
+            _threadCounterMutex.ReleaseMutex();
+        }
+    }
 }
